dd55bbcda2f1714062a529b95ae707c7b4598c21
madhvendra-singh <86185962+madhvendra-singh@users.noreply.github.com>
2021-06-23 10:44:16 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import time
-print("INFINITE MULTIPLIER")
-print("Write you first number")
-num1 = input()
-print("Write your second number")
-num2 = input()
-print("Sum of these numbers is", int(num1) * int(num2))
-time.sleep(10)
