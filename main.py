4839a3009f558121803c6a86dc087c3cd9c93c9b
madhvendra-singh <86185962+madhvendra-singh@users.noreply.github.com>
2021-06-23 10:44:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+print("INFINITE MULTIPLIER")
+print("Write you first number")
+num1 = input()
+print("Write your second number")
+num2 = input()
+print("Product of these numbers is", int(num1) * int(num2))
+time.sleep(10)
